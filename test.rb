882f7e8bf21e4b84d394c66ccfd6c87b036e13af
markrosos <chthonster@gmail.com>
2022-10-06 11:31:32 -0400

    test

---
+++ test.rb
@@ -0,0 +1,26 @@
+movies = { jaws: 1975,
+
+
+
+           anchorman: 2004,
+
+
+
+           man_of_steel: 2013,
+
+
+
+           a_beautiful_mind: 2001,
+
+
+
+           the_evil_dead: 1981 }
+
+
+
+puts movies[:jaws]
+
+
+
+fffffb ffffffffffff
+
